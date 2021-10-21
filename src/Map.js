888d6eb6fb8a04d385a54afdc479be31c333255e
DaltonChichester <Daltonchichester@icloud.com>
2021-10-21 02:13:39 -0400

    Switched to dark map theme

--- src/Map.js
+++ src/Map.js
@@ -14,16 +14,11 @@ import "esri-leaflet-geocoder/dist/esri-leaflet-geocoder.js";
 
 function MapComp({}) {
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "100vh", width: "100wh" }}>
+        <MapContainer center={[0, 0]} zoom={3} scrollWheelZoom={false} style={{ height: "100vh", width: "100wh" }}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
         </MapContainer>
     );
 }
