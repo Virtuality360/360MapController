db5e7d576a7f39e737b49ec8f073a68bedb21468
DaltonChichester <Daltonchichester@icloud.com>
2021-11-05 11:48:36 -0400

    Converted the rest of the project to only use functional components

--- src/PanoButton.js
+++ src/PanoButton.js
@@ -2,28 +2,28 @@ import React, {Component} from "react";
 import PannellumReact from './PannellumReact'
 import switchComponent from "./App"
 
-class ControllerButton extends React.Component {
-    handleClick = () => {
+function ControllerButton
+{
+    function handleClick()
+	{
         let self = this;
         self.props.toggleMap("Map");
     }
 
-    render() {
-        return ( 
-        	<div>
-        		<div style = {{position: 'absolute', top: '95px', left: '5px', zIndex: '1'}}>
-	            	<button onClick = {this.handleClick}>
-	            		Switch 
-	            	</button> 
-	            </div>
-	            <div>
-	            	<PannellumReact 
-	            		latLong = {this.props.latLong}
-	            	/>
-	            </div>
+    return ( 
+    	<div>
+    		<div style = {{position: 'absolute', top: '95px', left: '5px', zIndex: '1'}}>
+            	<button onClick = {this.handleClick}>
+            		Switch 
+            	</button> 
             </div>
-        );
-    }
+            <div>
+            	<PannellumReact 
+            		latLong = {this.props.latLong}
+            	/>
+            </div>
+        </div>
+    );
 }
 
 export default ControllerButton;
