e00275a771711e80d1b04b4b86dc950a280374d0
Dalton Chichester <daltonchichester@icloud.com>
2022-02-22 08:13:06 -0500

    Updated Map.jsx

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,48 +1,52 @@
-import React, { useEffect, useRef, Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup , CircleMarker, useMap} from 'react-leaflet'
-import { createControlComponent } from '@react-leaflet/core'
-import { Control } from 'leaflet'
-import { Images } from "./PanoConfigs/PanoConfigAll.json";
-import L, { LeafletMouseEvent, Map } from "leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, CircleMarker } from "react-leaflet";
+import { Images } from "./PanoConfigs/ProcessedPanoJSONLarge.json";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
 import "leaflet/dist/leaflet.css";
-import "esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
 
-import "leaflet/dist/leaflet.js";
-import "esri-leaflet-geocoder/dist/esri-leaflet-geocoder.js";
+const MapComp = (props) => {
+  const [map, setMap] = useState(null);
+  const [zoomLevel, setZoomLevel] = useState(props.zoom);
+  const [latLong, setlatLong] = useState(props.latLong);
 
-function MapComp(props)
-{
-  function renderMarkers()
-  {
-    //could be better
-    let latLong: Array<Array<any>> = [];
-    for (var i = 0; i < Images.length; i++) 
-    {
-      let temp: Array<any> = [];
-      temp.push(Images[i].Latitude, Images[i].Longitude);
-      latLong.push(temp);
-    }
+  let markers = [];
+  for (const { Latitude, Longitude } of Images) {
+    markers.push(
+      <CircleMarker
+        center={[Latitude, Longitude]}
+        eventHandlers={{
+          click: () => {
+            props.toggleMap("PanoViewer", [Latitude, Longitude], map.getZoom());
+          },
+        }}
+      ></CircleMarker>
+    );
+  }
 
-    return latLong;
-  };
+  return (
+    <MapContainer
+      className="markercluster-map"
+      center={latLong}
+      zoom={zoomLevel}
+      scrollWheelZoom={true}
+      whenCreated={setMap}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+      />
 
-  return(
-  <MapContainer center={[0, 0]} zoom={3} scrollWheelZoom={true} style={{ height: "100vh", width: "100wh" }}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
-    />
-    {renderMarkers().length > 0 &&
-      renderMarkers().map((value, index) => {
-        return (
-          <CircleMarker center={[value[0], value[1]]} key={index} eventHandlers={{
-            click: () => {props.toggleMap("PanoViewer", [value[0], value[1]])}}}>
-          </CircleMarker>
-        );
-     })}
-  </MapContainer>
-  )
-}
+      <MarkerClusterGroup
+        spiderfyDistanceMultiplier={1}
+        showCoverageOnHover={false}
+      >
+        {markers}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+};
 
 export default MapComp;
