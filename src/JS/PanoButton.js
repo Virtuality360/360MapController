69170736ba2752480b1b0f8baae968fb4c297681
Devlin Sypher <devlins@advanced-ent.com>
2022-07-07 13:39:26 -0400

    Corrected comment format

--- src/JS/PanoButton.js
+++ src/JS/PanoButton.js
@@ -9,12 +9,12 @@ const ControllerButton = (props) => {
   return (
     <div style={{height:"100%"}}>
     
-      //creates the buttom ontop of the PanoViewer to allow the switching back to the map
+      {/*creates the buttom ontop of the PanoViewer to allow the switching back to the map*/}
       <div style={{position: "relative", top: "150px", left: "5px", zIndex: "1",}}>
         <button onClick={handleClick}>Map</button>
       </div>
 
-      //creates the PanoViewer
+      {/*creates the PanoViewer*/}
       <div style={{height:"100%"}}>
          < PannellumReact latLong={props.latLong} zoom={props.zoom} />
       </div>
