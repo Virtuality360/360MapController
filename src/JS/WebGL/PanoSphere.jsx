1f1a91dc1a490823122572388eb4524083906c7b
Devlin Sypher <devlins@advanced-ent.com>
2022-10-06 14:51:38 -0400

    Parent hotspot to panosphere

--- src/JS/WebGL/PanoSphere.jsx
+++ src/JS/WebGL/PanoSphere.jsx
@@ -1,15 +1,20 @@
 import { useLoader } from '@react-three/fiber'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import * as THREE from 'three'
 
 import JSON from "../../PanoConfigs/demo-output.json";
 import PanoHotspot from './PanoHotspot';
 import { PANO_SPHERE_RADIUS } from "../../Constants/WebGL"
+import { useFrame } from "@react-three/fiber"
 
 function PanoSphere(props) {
+
     const [image, setImage] = useState(props.image)
     const colorMap = useLoader(THREE.TextureLoader, `/Images/${image}`)
+    const panoRef = useRef()
 
+    useFrame(() => (panoRef.current.rotation.y += 0.001))
+    
     function getHotspots() {
         console.log("image", image)
         for(const img of JSON.Images) {
@@ -19,31 +24,29 @@ function PanoSphere(props) {
         }
         return []   /** No match found */
     }
-
+    
     function loadHotspots() {
         const hotspots = getHotspots()
         let hotspotElements = []
         hotspots.forEach((hotspot) => hotspotElements.push(
             <PanoHotspot {...hotspot} key={hotspot.Path} handleClick={handleClick} />
-        ))
-        hotspotElements.push()
-        return hotspotElements
-    }
-
-    const handleClick = (path) => {
-        setImage(path)
-    }
-
-    const hotspots = loadHotspots()
+            ))
+            hotspotElements.push()
+            return hotspotElements
+        }
+        
+        const handleClick = (path) => {
+            setImage(path)
+        }
+        
+        const hotspots = loadHotspots()
     
     return(
-        <>
-        <mesh>
+        <mesh ref={panoRef}>
             <sphereGeometry args={[PANO_SPHERE_RADIUS, 500, 500]}/>
             <meshBasicMaterial map={colorMap} side={THREE.BackSide} />
+            {hotspots}
         </mesh>
-        {hotspots}
-        </>
     )
 }
 
